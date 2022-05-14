99699ae97124ddfaae0064cc19691acbc27eebfa
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-05-14 23:02:54 +0430

    added submit answers handler.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,8 @@
-import Login from './Login'
 import Questions from './Questions'
-import Signup from './Signup'
-
 
 const Main = () => {
   return (
     <main>
-      {/* <Login /> */}
-      {/* <Signup /> */}
       <Questions />
     </main>
   )
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,7 +6,8 @@ const questionsReducer = (state, action) => {
     case 'qusetionsFetched':
       return action.payload.data
     case 'answerSelected':
-      return state.map(item => item.id === action.payload.id ? {...item, selectedAnswer: action.payload.option} : item)
+      return state.map(item => item.id === action.payload.id ?
+        {...item, selectedAnswer: action.payload.option} : item)
     default:
       return state
   }
@@ -27,7 +28,11 @@ const positionCorrectAnswerIndexRandomly = (answers) => {
 
 const Questions = () => {
   const [questions, answerDispatch] = useReducer(questionsReducer, [])
-
+  const [status, setStatus] = useState({
+    isSubmitting: false,
+    isSubmitted: false
+  })
+  
   useEffect(() => {
     // put correct answer and incorrect answers together in an array
     const questions = data.map(q => ({
@@ -55,6 +60,19 @@ const Questions = () => {
     })
   }
   
+  const handleSubmit = () => {
+    setStatus({...status, isSubmitting: true})
+    let numberOfCorrectAns = 0
+    questions.forEach(q => {
+      if (q.answers.indexOf(q.correctAnswer) === q.selectedAnswer-1) {
+        numberOfCorrectAns++
+      }
+    });
+    
+    console.log("Correct answers: ", numberOfCorrectAns)
+    console.log("Incorrect answers: ", questions.length - numberOfCorrectAns)
+    setStatus({isSubmitting: false, isSubmitted: true})
+  }
   const elements = questions.map((item, index) => (
     <div className='question' key={item.id}>
       <p>
@@ -62,19 +80,55 @@ const Questions = () => {
         {item.question}
       </p>
       <ul className="answers">
-        <li onClick={() => handleSelectAnswer(item.id, '1')} >
+        <li 
+          onClick={() => handleSelectAnswer(item.id, '1')}
+          className={
+            !status.isSubmitted ? "" : (
+              item.answers.indexOf(item.correctAnswer) === 0 ? "correct-answer" : (
+                item.selectedAnswer-1 === 0 ? "wrong-answer" : ""
+              )
+            )
+          }
+        >
           <span className={item.selectedAnswer === '1' ? 'selected-answer' : ''}>1</span>
           {" "}{item.answers[0]}
           </li>
-        <li onClick={() => handleSelectAnswer(item.id, '2')}>
+        <li
+          onClick={() => handleSelectAnswer(item.id, '2')}
+          className={
+            !status.isSubmitted ? "" : (
+              item.answers.indexOf(item.correctAnswer) === 1 ? "correct-answer" : (
+                item.selectedAnswer-1 === 1 ? "wrong-answer" : ""
+              )
+            )
+          }
+        >
           <span className={item.selectedAnswer === '2' ? 'selected-answer' : ''}>2</span>
           {" "}{item.answers[1]}
         </li>
-        <li onClick={() => handleSelectAnswer(item.id, '3')}>
+        <li
+          onClick={() => handleSelectAnswer(item.id, '3')}
+          className={
+            !status.isSubmitted ? "" : (
+              item.answers.indexOf(item.correctAnswer) === 2 ? "correct-answer" : (
+                item.selectedAnswer-1 === 2 ? "wrong-answer" : ""
+              )
+            )
+          }
+        >
           <span className={item.selectedAnswer === '3' ? 'selected-answer' : ''}>3</span>
           {" "}{item.answers[2]}
         </li>
-        <li onClick={() => handleSelectAnswer(item.id, '4')}>
+        <li
+          onClick={() => handleSelectAnswer(item.id, '4')}
+          className={
+            !status.isSubmitted ? "" : (
+              item.answers.indexOf(item.correctAnswer) === 3 ? "correct-answer" : (
+                item.selectedAnswer-1 === 3 ? "wrong-answer" : ""
+              )
+            )
+          }
+        >
           <span className={item.selectedAnswer === '4' ? 'selected-answer' : ''}>4</span>
           {" "}{item.answers[3]}
         </li>
@@ -86,7 +140,9 @@ const Questions = () => {
       <h2>Python (20q)</h2>
       <div>
         { elements }
-        <button className='btn-submit'>Submit</button>
+        <button className='btn-submit' onClick={handleSubmit}>
+          {status.isSubmitting ? "Submitting..." : "Submit"}
+        </button>
       </div>
     </section>
   )
