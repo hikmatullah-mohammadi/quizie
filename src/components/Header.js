ca3019a2e2c6b340e5cd91d313e6a61e8c2c1c57
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-05-19 05:52:05 +0430

    added username and avatar to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,12 @@ export default () =>{
   return (
   <header>
     <h1>Quizie</h1>
+    <div className="user">
+      <div className="avatar">
+        <img src="./images/avatar.png" alt=""/>
+      </div>
+      <span className="username">hikmatullah_m80</span>
+    </div>
   </header>
   )
 }
