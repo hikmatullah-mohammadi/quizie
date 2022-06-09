3f38739b8942bef22fad39de1c54d3712776130f
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-06-09 22:08:07 +0430

    added loading indicator

---
+++ front-end/src/components/LoadingDisplay.js
@@ -0,0 +1,33 @@
+const LoadingDisplay = () => {
+  return (
+    <div className='loading-display'>
+      <div>
+        <p>Please wait..</p>
+        <div>
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingDisplay
