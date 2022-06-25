9fe35625ee329afdf0bd3dd6a0ca3e7c36310558
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-06-25 18:11:56 +0430

    code refactoring changed div to section html5 samantic

--- front-end/src/components/HomeNotLoggedIn.js
+++ front-end/src/components/HomeNotLoggedIn.js
@@ -5,37 +5,36 @@ const HomeNotLoggedIn = () => {
   const dispatch = useDispatch()
   return (
     <div className="home-not-logged-in">
-      <div className="welcome-msg">
-        <h3>Welcome :)</h3>
+      <section className="welcome-msg">
+        <h1>Welcome :)</h1>
         <p>Tons of multiple choice questions are available here.</p>
         <p>Assess your knowledge in your favorite subject at <span className="dollor">$0</span>.</p>
-      </div>
-      <div>
-        <div className="categories">
-          <h3>You can choose the following categories</h3>
-          <ul>
-            <li>Arts & Literature</li>
-            <li>Film & TV</li>
-            <li>Food & Drink</li>
-            <li>General Knowledge</li>
-            <li>Geography</li>
-            <li>History</li>
-            <li>Music</li>
-            <li>Science</li>
-            <li>Society & Culture</li>
-            <li>Sport & Leisure</li>
-          </ul>
-        </div>
-        <hr />
-        <div className="difficulties">
-          <h3>Three difficulty levels available </h3>
-          <ul>
-            <li>Easy</li>
-            <li>Medium</li>
-            <li>Hard</li>
-          </ul>
-        </div>
-      </div>
+      </section>
+
+      <section className="categories">
+        <h2>You can choose the following categories</h2>
+        <ul>
+          <li>Arts & Literature</li>
+          <li>Film & TV</li>
+          <li>Food & Drink</li>
+          <li>General Knowledge</li>
+          <li>Geography</li>
+          <li>History</li>
+          <li>Music</li>
+          <li>Science</li>
+          <li>Society & Culture</li>
+          <li>Sport & Leisure</li>
+        </ul>
+      </section>
+      
+      <section className="difficulties">
+        <h2>Three difficulty levels available </h2>
+        <ul>
+          <li>Easy</li>
+          <li>Medium</li>
+          <li>Hard</li>
+        </ul>
+      </section>
       <button
         className="btn-get-started"
         onClick={async () => {
