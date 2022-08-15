42cf7f35eaff5121f01c7661c5567709cc7e6d86
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-08-15 21:09:15 +0430

    got it ready to deploy on Heroku

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
+
 app.use('/api/v1/quizie', quiz)
 app.use('*', (req, res) => res.status(404).json({ error: "Not found" }))
 
